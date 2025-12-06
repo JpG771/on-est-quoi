f26b3a48d510d72f97e404194392b7389442c1e0
Jean-Pierre Gladu <jp.gladu@outlook.com>
2025-12-05 20:10:20 -0500

    Add accueil page

---
+++ src/app/accueil/accueil.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-accueil',
+  imports: [MatButtonModule],
+  templateUrl: './accueil.html',
+  styleUrl: './accueil.scss'
+})
+export class Accueil {
+
+}
