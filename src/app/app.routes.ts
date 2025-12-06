bd3926f2179de3146c6f6e34e70dd6dbcbbef66f
Jean-Pierre Gladu <jp.gladu@outlook.com>
2025-12-05 20:18:42 -0500

    Ajouter composant questionnaire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./accueil/accueil').then(m => m.Accueil),
     pathMatch: 'full'
+  },
+  {
+    path: 'questionnaire',
+    loadComponent: () => import('./questionnaire/questionnaire').then(m => m.Questionnaire)
   }
 ];
