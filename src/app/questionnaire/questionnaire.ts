90c11072481b66d1c76c1f1296ca56d87104dcb6
Jean-Pierre Gladu <jp.gladu@outlook.com>
2025-12-28 11:14:19 -0500

    Add questionnaire navigation

--- src/app/questionnaire/questionnaire.ts
+++ src/app/questionnaire/questionnaire.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Thermometre } from "../thermometre/thermometre";
+import { Contrat } from "../contrat/contrat";
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-questionnaire',
-  imports: [Thermometre],
+  imports: [Thermometre, Contrat, MatButtonModule, MatStepperModule],
   templateUrl: './questionnaire.html',
   styleUrl: './questionnaire.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class Questionnaire {
 
