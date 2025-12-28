a194970ace40f9a7a4b007579ec5cd89b684a8d4
Jean-Pierre Gladu <jp.gladu@outlook.com>
2025-12-27 21:26:26 -0500

    Ajout du composant Contrat vide

---
+++ src/app/contrat/contrat.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-contrat',
+  imports: [MatExpansionModule],
+  templateUrl: './contrat.html',
+  styleUrl: './contrat.scss',
+})
+export class Contrat {
+
+}
